a97890c7d6a49bac83e6b0e47e193f09a48d8533
Harold Kimsvg/onload=alert(1) <me@harold.kim>
2021-02-13 19:14:52 -0800

    Modify a bit

--- clubhouse.py
+++ clubhouse.py
@@ -11,8 +11,10 @@ Please know what you're doing!
 Modifying a bit of header could result a permanent block on your account.
 """
 
-import requests
+import uuid
 import time
+import functools
+import requests
 
 class Clubhouse:
     """
@@ -23,12 +25,12 @@ class Clubhouse:
     API_HOST = "www.clubhouseapi.com"
     API_BUILD_ID = "297"
     API_BUILD_VERSION = "0.1.27"
+    API_UA = "clubhouse/297 (iPhone; iOS 13.5.1; Scale/3.00)"
 
     # User information
     API_USER_ID = None
     API_USER_TOKEN = None
     API_USER_DEVICE = None # str(__import__("uuid").uuid4())
-    API_USER_AGENT = "clubhouse/297 (iPhone; iOS 13.5.1; Scale/3.00)"
 
     # Some useful information for commmunication
     PUBNUB_PUB_KEY = "pub-c-6878d382-5ae6-4494-9099-f930f938868b"
@@ -44,21 +46,44 @@ class Clubhouse:
 
     # Useful header information
     HEADERS = {
-        "Authorization": f"Token {API_USER_TOKEN}",
         "CH-Languages": "en-JP,ja-JP",
-        "CH-UserID": "(null)" if API_USER_ID is None else API_USER_ID,
         "CH-Locale": "en_JP",
         "Accept": "application/json",
         "Accept-Language": "en-JP;q=1, ja-JP;q=0.9",
         "Accept-Encoding": "gzip, deflate",
         "CH-AppBuild": f"{API_BUILD_ID}",
         "CH-AppVersion": f"{API_BUILD_VERSION}",
-        "CH-DeviceId": f"{API_USER_DEVICE}",
-        "User-Agent": f"{API_USER_AGENT}",
+        "User-Agent": f"{API_UA}",
         "Connection": "close",
         "Content-Type": "application/json; charset=utf-8",
         "Cookie": "__cfduid=def2c5e4b7f2e3ba18ab49625408b043d1613200581"
     }
+
+
+    def require_authentication(func):
+        """ Simple decorator to check for the authentication """
+        @functools.wraps(func)
+        def wrap(self, *args, **kwargs):
+            if self.API_USER_ID is None or self.API_USER_TOKEN is None:
+                raise Exception('Not Authenticated')
+            return func(self, *args, **kwargs)
+        return wrap
+
+
+    def __init__(self, user_id='', user_token='', user_device=''):
+        """ (Clubhouse, str, str, str)
+
+        Set default information
+        """
+        # TODO: change design..
+        self.API_USER_ID = user_id if user_id else None
+        self.API_USER_TOKEN = user_token if user_token else None
+        self.API_USER_DEVICE = user_device if user_device else uuid.uuid4()
+
+        # Update code
+        self.HEADERS['CH-UserID'] = "(null)" if self.API_USER_ID is None else self.API_USER_ID
+        self.HEADERS['Authorization'] = f"Token {self.API_USER_TOKEN}",
+        self.HEADERS['CH-DeviceId'] = self.API_USER_DEVICE
 
 
     def start_phone_number_auth(self, phone_number):
@@ -71,6 +96,8 @@ class Clubhouse:
             +818043211234
             ...
         """
+        if self.API_USER_ID is not None:
+            raise Exception('Already Authenticatied')
         data = {
             "phone_number": phone_number
         }
@@ -86,14 +113,18 @@ class Clubhouse:
 
         Please note that output may be different depending on the status of the authenticated user
         """
+        if self.API_USER_ID is not None:
+            raise Exception('Already Authenticatied')
         data = {
             "phone_number": phone_number,
             "verification_code": verification_code
         }
         req = requests.post(f"https://{self.API_HOST}/api/complete_phone_number_auth", headers=self.HEADERS, json=data)
+        # find out how userId is taken
         return req.json()
 
 
+    @require_authentication
     def join_channel(self, channel, attribution_source="feed"):
         """ (Clubhouse, str, str) -> dict
 
@@ -109,6 +140,7 @@ class Clubhouse:
         return req.json()
 
 
+    @require_authentication
     def leave_channel(self, channel):
         """ (Clubhouse, str) -> dict
 
@@ -122,6 +154,7 @@ class Clubhouse:
         return req.json()
 
 
+    @require_authentication
     def get_profile(self, user_id):
         """ (Clubhouse, str) -> dict
 
@@ -134,6 +167,7 @@ class Clubhouse:
         return req.json()
 
 
+    @require_authentication
     def get_profile_self(self, return_blocked_ids=False, timezone_identifier="Asia/Tokyo", return_following_ids=False):
         """ (Clubhouse, bool, str, bool) -> dict
 
@@ -149,6 +183,7 @@ class Clubhouse:
         return req.json()
 
 
+    @require_authentication
     def get_profile_following(self, user_id):
         """ (Clubhouse, str) -> dict
 
@@ -161,6 +196,7 @@ class Clubhouse:
         return req.json()
 
 
+    @require_authentication
     def get_all_topics(self):
         """ (Clubhouse) -> dict
 
@@ -170,6 +206,7 @@ class Clubhouse:
         return req.json()
 
 
+    @require_authentication
     def get_channels(self):
         """ (Clubhouse) -> dict
 
@@ -179,6 +216,7 @@ class Clubhouse:
         return req.json()
 
 
+    @require_authentication
     def active_ping(self, channel):
         """ (Clubhouse, str) -> dict
 
@@ -192,6 +230,7 @@ class Clubhouse:
         return req.json()
 
 
+    @require_authentication
     def audience_reply(self, channel, raise_hands=True, unraise_hands=False):
         """ (Clubhouse, str, bool, bool) -> bool
 
@@ -206,6 +245,7 @@ class Clubhouse:
         return req.json()
 
 
+    @require_authentication
     def update_skintone(self, skintone=1):
         """ (Clubhouse, int) -> dict
 
@@ -222,6 +262,7 @@ class Clubhouse:
         return req.json()
 
 
+    @require_authentication
     def get_notifications(self, page_size=20, page=1):
         """ (Clubhouse, int, int) -> dict
 
@@ -232,6 +273,7 @@ class Clubhouse:
         return req.json()
 
 
+    @require_authentication
     def get_online_friends(self):
         """ (Clubhouse) -> dict
 
@@ -241,6 +283,7 @@ class Clubhouse:
         return req.json()
 
 
+    @require_authentication
     def accept_speaker_invite(self, channel, user_id):
         """ (Clubhouse, str, int) -> dict
 
@@ -255,6 +298,7 @@ class Clubhouse:
         return req.json()
 
 
+    @require_authentication
     def get_suggested_speakers(self, channel):
         """ (Clubhouse, str) -> dict
 
@@ -267,6 +311,7 @@ class Clubhouse:
         return req.json()
 
 
+    @require_authentication
     def create_channel(self, topic="", user_ids=(), is_private=False, is_social_mode=False):
         """ (Clubhouse, str, list, bool, bool) -> dict
 
@@ -284,6 +329,7 @@ class Clubhouse:
         return req.json()
 
 
+    @require_authentication
     def get_create_channel_targets(self):
         """ (Clubhouse) -> dict
 
@@ -295,16 +341,16 @@ class Clubhouse:
 
 
 if __name__ == "__main__":
-    clubhouse = Clubhouse()
+    CLUBHOUSE = Clubhouse()
 
-    print(clubhouse.get_channels())
+    print(CLUBHOUSE.get_channels())
     CURR_CHANNEL = "CHANGEME"
-    print(clubhouse.join_channel(CURR_CHANNEL))
+    print(CLUBHOUSE.join_channel(CURR_CHANNEL))
     time.sleep(4)
-    print(clubhouse.leave_channel(CURR_CHANNEL))
+    print(CLUBHOUSE.leave_channel(CURR_CHANNEL))
     print("Clubhouse API worked successfully")
 
     # Check for authentication
-    # if API_USER_ID != "CHANGEME":
+    # if self.API_USER_ID != "CHANGEME":
     # self.HEADERS['CH-UserID'] = f"{API_USER_ID}"
     # self.HEADERS['Authorization'] = f"{API_USER_TOKEN}"
