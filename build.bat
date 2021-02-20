599ea87f000c608b21a308a94baccd22275778a6
Harold Kim <me@harold.kim>
2021-02-20 14:33:59 +0900

    Use different hotkeys depending on type of OS

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 
-python -m PyInstaller --onefile .\cli.py --onefile --icon=icon.ico
+python -m PyInstaller --onefile .\cli.py --icon=icon.ico
---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python3 -m PyInstaller --onefile ./cli.py
